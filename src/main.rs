444db5cb3f8af51132dcedc649bb91fc57b0b716
takahiro hagiuda <jb@takahiros-MacBook-Air.local>
2024-05-09 07:06:39 +0900

    feat: implement new command, add src_files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use std::fs::File;
+use std::io::Write;
 use std::path::Path;
 use std::process::Command;
 
@@ -35,10 +37,28 @@ fn create_project(contest_number: &str) {
         if let Err(err) = create_cargo_project(project_dir) {
             eprintln!("Failed to create Cargo project: {}", err);
         }
+
+        if let Err(err) = create_src_files(project_dir) {
+            eprintln!("Failed to create source files: {}", err);
+        }
     }
 }
 
 fn create_cargo_project(project_dir: &Path) -> std::io::Result<()> {
     Command::new("cargo").arg("new").arg(project_dir).status()?;
+    Ok(())
+}
+
+fn create_src_files(project_dir: &Path) -> std::io::Result<()> {
+    let src_dir = project_dir.join("src");
+
+    for c in b'a'..b'h' {
+        let filename = format!("{}.rs", c as char);
+        let filepath = src_dir.join(filename);
+
+        let mut file = File::create(filepath)?;
+        file.write_all(b"fn main() {\n    println!(\"Hello, world!\");\n}\n")?;
+    }
+
     Ok(())
 }
