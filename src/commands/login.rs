66eb29a6798105db9547af8afd0d37a4ce6d348d
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-09 23:24:09 +0900

    feat: login command, get username and password from user prompt

--- src/commands/login.rs
+++ src/commands/login.rs
@@ -1,3 +1,21 @@
+use promptuity::{
+    prompts::{Input, Password},
+    themes::FancyTheme,
+    Promptuity, Term,
+};
+
 pub fn execute() {
-    println!("login command!");
+    let mut term = Term::default();
+    let mut theme = FancyTheme::default();
+    let mut p = Promptuity::new(&mut term, &mut theme);
+
+    let _ = p.term().clear();
+    let _ = p.with_intro("Login AtCoder!").begin();
+
+    let name: Result<String, promptuity::Error> =
+        p.prompt(Input::new("Please enter your username").with_placeholder("username"));
+    let pass: Result<String, promptuity::Error> =
+        p.prompt(Password::new("Please enter your password").with_required(false));
+
+    println!("username = {:?}, password = {:?}", name, pass);
 }
